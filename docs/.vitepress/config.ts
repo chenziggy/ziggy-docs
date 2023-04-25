e4405f453d2b8d718f90085606747a6977a1fab7
Ziggy Chen <ziggycs9796@hotmail.com>
2023-04-25 11:09:18 +0800

    feat: css mask

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,44 +1,47 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: 'ziggy-docs',
-  description: 'Z master document',
+  title: "ziggy-docs",
+  description: "Z master document",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Book', link: '/computer/byte-order' }
+      { text: "Home", link: "/" },
+      { text: "Book", link: "/computer/byte-order" },
     ],
 
     sidebar: [
       {
-        text: 'Computer',
+        text: "Computer",
         items: [
-          { text: '字节序', link: '/computer/byte-order' },
-          { text: '位图', link: '/computer/bitmap' }
-        ]
+          { text: "字节序", link: "/computer/byte-order" },
+          { text: "位图", link: "/computer/bitmap" },
+        ],
       },
       {
-        text: 'Frontend',
-        items: [{ text: 'keyboardEvent', link: '/frontend/keyboardEvent' }]
+        text: "Frontend",
+        items: [
+          { text: "keyboardEvent", link: "/frontend/keyboardEvent" },
+          { text: "mask", link: "/frontend/mask" },
+        ],
       },
       {
-        text: 'Node',
-        items: [{ text: 'process.nextTick', link: '/node/nextTick' }]
+        text: "Node",
+        items: [{ text: "process.nextTick", link: "/node/nextTick" }],
       },
       {
-        text: 'C',
-        items: [{ text: 'FFERRTAG', link: '/c/fferrtag' }]
+        text: "C",
+        items: [{ text: "FFERRTAG", link: "/c/fferrtag" }],
       },
       {
-        text: 'Examples',
-        items: [{ text: 'Runtime API Examples', link: '/api-examples' }]
-      }
+        text: "Examples",
+        items: [{ text: "Runtime API Examples", link: "/api-examples" }],
+      },
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+  },
+});
---
+++ docs/frontend/mask.md
@@ -0,0 +1,40 @@
+# mask
+
+在查看**mdn**文档时，会经常看见废弃语法，废弃语法的标识是一个粉色垃圾桶<span class="icon icon-deprecated"></span>  
+本以为他是一个检点`<img>`标签，出于好奇打开控制台发现并非如此
+
+* `mask-image`: none
+* `mask-repeat`: repeat
+* `mask-position`: center
+* `mask-clip`: border-box
+* `mask-origin`: border-box
+* `mask-size`: auto
+* `mask-composite`: add
+
+## mask-image
+
+设置用作元素遮罩层的图像。遮罩图像会按照 `mask-mode`<span class="icon icon-experimental"></span>   和 `mask-position` 等属性设置进行缩放和平移，最后与元素背景叠加形成遮罩效果。  
+个人理解为镂空图形，有颜色的地方都是空心的。遮罩图像在上，原图或背景颜色在下
+
+`mask-image`的属性值：url()|渐变|image()|element  
+图片格式有所要求，无透明通道的图像会看不见原图 jpg无透明通道
+
+## 应用
+改变icon颜色
+```css
+.icon-deprecated {
+  -webkit-mask-image: url(/img/deprecated.svg);
+  mask-image: url(/img/deprecated.svg);
+  background-color: #ff707f;
+}
+
+/*
+  粉色垃圾桶
+  deprecated.svg 是一个黑色垃圾桶 作为遮罩层图像
+  background-color: #ff707f; 设置粉色背景
+*/
+```
+
+<style>
+  @import '/css/common.css'
+</style>
