56675b35f47b9823f4aef916452286c567172ce0
Ziggy Chen <ziggycs9796@hotmail.com>
2023-08-14 16:29:35 +0800

    feat: mergeSort

--- js/sort.spec.js
+++ js/sort.spec.js
@@ -1,5 +1,6 @@
 import { describe, expect, it } from 'vitest'
 import { quickSort } from './quickSort'
+import { mergeSort } from './mergeSort'
 
 describe('排序', () => {
   it('quickSort', () => {
@@ -7,5 +8,11 @@ describe('排序', () => {
     const sortArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9]
     quickSort(arr, 0, arr.length - 1)
     expect(arr).toEqual(sortArr)
+  })
+
+  it('mergeSort', () => {
+    const arr = [9, 8, 7, 6, 5, 4, 3, 1, 2]
+    const sortArr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    expect(mergeSort(arr)).toEqual(sortArr)
   })
 })
